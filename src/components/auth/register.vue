d98b5e31036ff0e7de9e306f3b2ef43efb5d42f4
Dale de Kock <ddk2707@gmail.com>
2022-03-28 13:49:58 +0200

    login/register attempts

--- src/components/auth/register.vue
+++ src/components/auth/register.vue
@@ -4,7 +4,7 @@
       <div class="row">
         <div class="col-lg-6 offset-lg-3 col-sm-10 offset-sm-1">
             <form
-            class="text-center border border-primary p-5"
+            class="border border-primary p-5"
             style="margin-top:70px;height:auto;padding-top:100px !important;"
             @submit.prevent="registerUser">
 
@@ -30,7 +30,7 @@
             v-model="register.password"
             />
 
-            <select name="roles" id="roleselect">
+            <select name="roles" id="roleselect" v-model="register.role">
                 <option value="user/artist">Artist</option>
                 <option value="producer"> Producer</option>
                 <option value="exec/label">Music Label</option>
@@ -58,7 +58,8 @@ export default {
       register: {
         username:"",
         email: "",
-        password: ""
+        password: "",
+        role:""
       }
     };
   },
